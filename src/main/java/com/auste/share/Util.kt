2e4f51628083800476ad0acb54458fa51e187cd2
austenYang <austenyad@gmail.com>
2023-10-31 01:46:38 +0800

    Squashed 'share_module/' changes from 2c806f0..7958439
    7958439 Merge commit '1a8aa09beb7c3af223ea054ce0ab5f5cfab05091'
    2207faf 添加减法
    ca51494 Merge commit '7a1e4e1943bc1280d3a8580fcb3bc57ed3ad0dd9'
    0627274 添加子模块完成并测试
    2c3281f Merge commit '830bccbcca6e32618e3cf0228adbabf3513d0a8c' as 'share_module'
    26c0dbc 加入模块前提交
    885d70a 添加子module
    a76eac2 first commit
    
    git-subtree-dir: share_module
    git-subtree-split: 7958439c5c51ec8ecc6109a0d8f447cc8ad82a69

---
+++ src/main/java/com/auste/share/Util.kt
@@ -0,0 +1,21 @@
+package com.auste.share
+
+/**
+ * Company: TAO_LE
+ * ================================================
+ * Description:
+ * <p>
+ * <p>
+ * Created by austenYang on 2023/10/31
+ * ================================================
+ */
+object Util {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun minus(a: Int, b: Int): Int {
+        return a - b
+    }
+}
